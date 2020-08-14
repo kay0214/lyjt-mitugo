633bcbbe861f3d0304836545816b152fa7b2b0de
zhangqingqing <zhangqingqing@hyjf.com>
2020-08-14 15:46:56 +0800

    分享人用户ID

--- yshop-api/src/main/java/co/yixiang/modules/order/entity/YxStoreOrder.java
+++ yshop-api/src/main/java/co/yixiang/modules/order/entity/YxStoreOrder.java
@@ -184,6 +184,9 @@ public class YxStoreOrder extends BaseEntity {
     @ApiModelProperty(value = "推荐人用户ID")
     private Integer parentId;
 
+    @ApiModelProperty(value = "分享人用户ID")
+    private Integer shareId;
+
     @ApiModelProperty(value = "推荐人类型:1商户;2合伙人;3用户")
     private Integer parentType;
 
--- yshop-api/src/main/java/co/yixiang/modules/order/web/vo/YxStoreOrderQueryVo.java
+++ yshop-api/src/main/java/co/yixiang/modules/order/web/vo/YxStoreOrderQueryVo.java
@@ -183,6 +183,9 @@ public class YxStoreOrderQueryVo implements Serializable {
     @ApiModelProperty(value = "推荐人用户ID")
     private Integer parentId;
 
+    @ApiModelProperty(value = "分享人用户ID")
+    private Integer shareId;
+
     @ApiModelProperty(value = "推荐人类型:1商户;2合伙人;3用户")
     private Integer parentType;
 
