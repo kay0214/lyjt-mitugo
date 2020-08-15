b90796409dbdb784672106db318db998b214debe
zhangqingqing <zhangqingqing@hyjf.com>
2020-08-15 10:25:06 +0800

    添加上级id字段

--- yshop-api/src/main/java/co/yixiang/modules/manage/entity/SystemUser.java
+++ yshop-api/src/main/java/co/yixiang/modules/manage/entity/SystemUser.java
@@ -2,7 +2,10 @@ package co.yixiang.modules.manage.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import co.yixiang.common.entity.BaseEntity;
+
+import java.math.BigDecimal;
 import java.util.Date;
+
 import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -21,65 +24,68 @@ import java.util.Date;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ApiModel(value="User对象", description="系统用户")
+@ApiModel(value = "User对象", description = "系统用户")
 public class SystemUser extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
-@ApiModelProperty(value = "ID")
-@TableId(value = "id", type = IdType.AUTO)
-private Long id;
+    @ApiModelProperty(value = "ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
 
-@ApiModelProperty(value = "头像")
-private Long avatarId;
+    @ApiModelProperty(value = "头像")
+    private Long avatarId;
 
-@ApiModelProperty(value = "邮箱")
-private String email;
+    @ApiModelProperty(value = "邮箱")
+    private String email;
 
-@ApiModelProperty(value = "状态：1启用、0禁用")
-private Long enabled;
+    @ApiModelProperty(value = "状态：1启用、0禁用")
+    private Long enabled;
 
-@ApiModelProperty(value = "密码")
-private String password;
+    @ApiModelProperty(value = "密码")
+    private String password;
 
-@ApiModelProperty(value = "用户名")
-private String username;
+    @ApiModelProperty(value = "用户名")
+    private String username;
 
-@ApiModelProperty(value = "部门名称")
-private Long deptId;
+    @ApiModelProperty(value = "部门名称")
+    private Long deptId;
 
-@ApiModelProperty(value = "手机号码")
-private String phone;
+    @ApiModelProperty(value = "手机号码")
+    private String phone;
 
-@ApiModelProperty(value = "岗位名称")
-private Long jobId;
+    @ApiModelProperty(value = "岗位名称")
+    private Long jobId;
 
-@ApiModelProperty(value = "创建日期")
-private Date createTime;
+    @ApiModelProperty(value = "创建日期")
+    private Date createTime;
 
-@ApiModelProperty(value = "最后修改密码的日期")
-private Date lastPasswordResetTime;
+    @ApiModelProperty(value = "最后修改密码的日期")
+    private Date lastPasswordResetTime;
 
-private String nickName;
+    private String nickName;
 
-private String sex;
+    private String sex;
 
-@ApiModelProperty(value = "用户角色：0->平台运营,1->合伙人,2->商户")
-private Integer userRole;
+    @ApiModelProperty(value = "用户角色：0->平台运营,1->合伙人,2->商户")
+    private Integer userRole;
 
-@ApiModelProperty(value = "商户联系人")
-private String merchantsContact;
+    @ApiModelProperty(value = "上级id")
+    private Integer parentId;
 
-@ApiModelProperty(value = "联系电话")
-private String contactPhone;
+    @ApiModelProperty(value = "商户联系人")
+    private String merchantsContact;
 
-@ApiModelProperty(value = "商户状态：0->启用,1->禁用")
-private Integer merchantsStatus;
+    @ApiModelProperty(value = "联系电话")
+    private String contactPhone;
 
-@ApiModelProperty(value = "总积分")
-private Integer totalScore;
+    @ApiModelProperty(value = "商户状态：0->启用,1->禁用")
+    private Integer merchantsStatus;
 
-@ApiModelProperty(value = "推荐用二维码地址")
-private String qrCodeUrl;
+    @ApiModelProperty(value = "总积分")
+    private BigDecimal totalScore;
+
+    @ApiModelProperty(value = "推荐用二维码地址")
+    private String qrCodeUrl;
 
 }
