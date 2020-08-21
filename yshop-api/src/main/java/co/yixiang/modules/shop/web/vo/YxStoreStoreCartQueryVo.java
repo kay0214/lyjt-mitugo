18a2ece3bf044eeb390cfec58cc07bba4ab13ba5
nixiaoling <nixiaoling@hyjf.com>
2020-08-21 14:42:33 +0800

    api新加代码提交（商品）

---
+++ yshop-api/src/main/java/co/yixiang/modules/shop/web/vo/YxStoreStoreCartQueryVo.java
@@ -0,0 +1,32 @@
+package co.yixiang.modules.shop.web.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * <p>
+ * 购物车表 查询结果对象
+ * </p>
+ *
+ * @author hupeng
+ * @date 2019-10-25
+ */
+@Data
+@ApiModel(value = "YxStoreStoreCartQueryVo对象", description = "购物车参数")
+public class YxStoreStoreCartQueryVo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "店铺id")
+    private Integer storeId;
+
+    @ApiModelProperty(value = "店铺名称")
+    private String storeName;
+
+    @ApiModelProperty(value = "购物车商品信息")
+    private List<YxStoreCartQueryVo> cartList;
+
+}
