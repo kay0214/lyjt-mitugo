06e779a8ddfa098ade62b6ee4476d12cbbff954f
huanghui <huanghui@hyjf.com>
2020-08-26 16:42:12 +0800

    C端 - 卡券 订单确认和订单创建

--- yshop-api/src/main/java/co/yixiang/modules/coupons/service/YxCouponOrderService.java
+++ yshop-api/src/main/java/co/yixiang/modules/coupons/service/YxCouponOrderService.java
@@ -1,12 +1,18 @@
 package co.yixiang.modules.coupons.service;
 
-import co.yixiang.modules.coupons.entity.YxCouponOrder;
 import co.yixiang.common.service.BaseService;
-import co.yixiang.modules.coupons.web.param.YxCouponOrderQueryParam;
-import co.yixiang.modules.coupons.web.vo.YxCouponOrderQueryVo;
 import co.yixiang.common.web.vo.Paging;
+import co.yixiang.modules.coupons.entity.YxCouponOrder;
+import co.yixiang.modules.coupons.web.param.YxCouponOrderQueryParam;
+import co.yixiang.modules.coupons.web.vo.CouponOrderQueryVo;
+import co.yixiang.modules.coupons.web.vo.YxCouponOrderQueryVo;
+import co.yixiang.modules.coupons.web.vo.YxCouponsQueryVo;
+import co.yixiang.modules.order.web.dto.CouponCacheDTO;
+import co.yixiang.modules.order.web.dto.PriceGroupDTO;
+import co.yixiang.modules.order.web.param.OrderParam;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * <p>
@@ -31,5 +37,46 @@ public interface YxCouponOrderService extends BaseService<YxCouponOrder> {
      * @return
      */
     Paging<YxCouponOrderQueryVo> getYxCouponOrderPageList(YxCouponOrderQueryParam yxCouponOrderQueryParam) throws Exception;
+
+    PriceGroupDTO getOrderPriceGroup(CouponOrderQueryVo couponOrderQueryVo);
+
+    Double getOrderSumPrice(CouponOrderQueryVo couponOrderQueryVo, String key);
+
+    String cacheOrderInfo(int uid, List<YxCouponsQueryVo> couponsQueryVoList, PriceGroupDTO priceGroup);
+
+    /**
+     * 卡券创建订单
+     * @param uid
+     * @param key
+     * @param param
+     * @return
+     */
+    YxCouponOrder createOrder(int uid, String key, OrderParam param);
+
+    CouponCacheDTO getCacheOrderInfo(int uid, String key);
+
+    /**
+     * 删除缓存
+     * @param uid
+     * @param key
+     */
+    void delCacheOrderInfo(int uid, String key);
+
+    /**
+     * 余额支付
+     * @param orderId
+     * @param uid
+     */
+    void yuePay(String orderId, int uid);
+
+    /**
+     * 获取订单信息
+     * @param unique
+     * @param uid
+     * @return
+     */
+    YxCouponOrderQueryVo getOrderInfo(String unique, int uid);
+
+    void paySuccess(String orderId, String payType);
 
 }
---
+++ yshop-api/src/main/java/co/yixiang/modules/order/web/dto/CouponCacheDTO.java
@@ -0,0 +1,16 @@
+package co.yixiang.modules.order.web.dto;
+
+import co.yixiang.modules.coupons.web.vo.YxCouponsQueryVo;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Author : huanghui
+ */
+@Data
+public class CouponCacheDTO {
+
+    private List<YxCouponsQueryVo> couponsQueryVoList;
+    private PriceGroupDTO priceGroup;
+}
