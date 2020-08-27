c9639a148edb43f69bc36c069a7edb6982d71738
nixiaoling <nixiaoling@hyjf.com>
2020-08-27 18:41:15 +0800

    index修改

--- yshop-api/src/main/java/co/yixiang/modules/shop/web/controller/IndexController.java
+++ yshop-api/src/main/java/co/yixiang/modules/shop/web/controller/IndexController.java
@@ -23,7 +23,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +51,7 @@ public class IndexController {
 
 
     @AnonymousAccess
-    @Cacheable(cacheNames = ShopConstants.YSHOP_REDIS_INDEX_KEY)
+//    @Cacheable(cacheNames = ShopConstants.YSHOP_REDIS_INDEX_KEY)
     @GetMapping("/index")
     @ApiOperation(value = "首页数据",notes = "首页数据")
     public ApiResult<Map<String,Object>> index(){
