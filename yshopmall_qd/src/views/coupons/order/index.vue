fe0f79e0fc042719f500dc03dcbfcb27013328ff
cuijianan <cuijianan@hyjf.com>
2020-08-30 19:54:02 +0800

    订单管理-卡券订单管理优化

--- yshopmall_qd/src/views/coupons/order/index.vue
+++ yshopmall_qd/src/views/coupons/order/index.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <div class="container">
       <el-tabs v-model="orderStatus" type="card" @tab-click="handleOrder">
-        <el-tab-pane name="-9">
+        <el-tab-pane name="">
           <span slot="label"><i class="el-icon-s-order"></i> 全部订单</span>
         </el-tab-pane>
           <el-tab-pane 
@@ -17,7 +17,7 @@
         <div>
           <!-- 搜索 -->
           <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
-          <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
+          <el-select v-model="query.orderType" clearable placeholder="类型" class="filter-item" style="width: 130px">
             <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
           <el-date-picker
