6b3d94a89cebb5e656baca48cb3a5be22fd67dbc
cuijianan <cuijianan@hyjf.com>
2020-08-31 14:53:47 +0800

    卡券分类-添加名称搜索

--- yshopmall_qd/src/views/coupons/category/index.vue
+++ yshopmall_qd/src/views/coupons/category/index.vue
@@ -3,6 +3,10 @@
     <!--工具栏-->
     <div class="head-container">
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <div>
+        <el-input v-model="query.cateName" clearable placeholder="分类名称" style="width: 130px;" class="filter-item" />
+        <rrOperation :crud="crud" />
+      </div>
       <crudOperation :permission="permission" />
       <!--表单组件-->
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
