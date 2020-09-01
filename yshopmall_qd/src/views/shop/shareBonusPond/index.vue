1633ba52bf2946c66ec8f632abe1d19bbb38d5e2
cuijianan <cuijianan@hyjf.com>
2020-09-01 16:24:49 +0800

    财务管理-拉新池绑定数据

--- yshopmall_qd/src/views/shop/shareBonusPond/index.vue
+++ yshopmall_qd/src/views/shop/shareBonusPond/index.vue
@@ -17,14 +17,13 @@
         </el-date-picker>
         <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="crud.toQuery">搜索</el-button>
       </el-row>
-      <el-row :gutter='6' style="margin:20px;"><el-col :span='4'>累计总金额: <span>0</span></el-col><el-col :span='4'>剩余金额: <span>0</span></el-col></el-row>
+      <el-row :gutter='6' style="margin:20px;"><el-col :span='4'>累计总金额: <span>{{totalAmount}}</span></el-col></el-row>
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="index" width="55" />
         <el-table-column v-if="columns.visible('username')" prop="username" label="用户昵称" />
         <el-table-column v-if="columns.visible('orderId')" prop="orderId" label="订单编号" />
-        <el-table-column v-if="columns.visible('orderPrice')" prop="orderPrice" label="明细种类" />
-        <el-table-column v-if="columns.visible('commission')" prop="commission" label="金额" />
+        <el-table-column v-if="columns.visible('orderPrice')" prop="orderPrice" label="金额" />
         <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="订单日期">
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.createTime) }}</span>
@@ -55,35 +54,21 @@ export default {
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
   data() {
     return {
-      
+      totalAmount:0,//累计金额
       permission: {
         add: ['admin', 'yxPointDetail:add'],
         edit: ['admin', 'yxPointDetail:edit'],
         del: ['admin', 'yxPointDetail:del']
       },
       rules: {
-        uid: [
-          { required: true, message: '用户ID不能为空', trigger: 'blur' }
-        ],
-        type: [
-          { required: true, message: '积分类别 0:拉新 1:分红不能为空', trigger: 'blur' }
-        ],
-        merchantsId: [
-          { required: true, message: '商户id不能为空', trigger: 'blur' }
-        ],
-        partnerId: [
-          { required: true, message: '合伙人id不能为空', trigger: 'blur' }
-        ],
-        delFlag: [
-          { required: true, message: '是否删除（0：未删除，1：已删除）不能为空', trigger: 'blur' }
-        ],
-        createTime: [
-          { required: true, message: '创建时间不能为空', trigger: 'blur' }
-        ],
-        updateTime: [
-          { required: true, message: '更新时间不能为空', trigger: 'blur' }
-        ]
-      }    }
+      }    
+    }
+  },
+  mounted(){
+    //获取累计金额
+    this.crud.refresh().then(res=>{
+      this.totalAmount=res.totalAmount
+    })
   },
   watch: {
   },
