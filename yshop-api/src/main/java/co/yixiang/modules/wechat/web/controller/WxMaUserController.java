ea1b339602d156ba7ed2b98fc8c28f90f3ce2f07
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-09-04 16:37:02 +0800

    【蜜兔】  再增加打印日志

--- yshop-api/src/main/java/co/yixiang/modules/wechat/web/controller/WxMaUserController.java
+++ yshop-api/src/main/java/co/yixiang/modules/wechat/web/controller/WxMaUserController.java
@@ -82,6 +82,7 @@ public class WxMaUserController {
     @PostMapping("/wxapp/binding")
     @ApiOperation(value = "小程序绑定手机号", notes = "小程序绑定手机号")
     public ApiResult<Map<String, Object>> phone(@Validated @RequestBody WxPhoneParam param) {
+        log.info("授权手机号：" + JSONObject.toJSONString(param));
         int uid = SecurityUtils.getUserId().intValue();
         YxUserQueryVo userQueryVo = userService.getYxUserById(uid);
         if (StrUtil.isNotBlank(userQueryVo.getPhone())) {
