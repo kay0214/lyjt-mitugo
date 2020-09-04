217aa9d965a5dea5ff56f0e41c5dcd24e026810a
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-09-04 11:48:20 +0800

    【蜜兔】 查询卡券列表增加分类查询

--- yshop-api/src/main/java/co/yixiang/modules/coupons/web/param/YxCouponsQueryParam.java
+++ yshop-api/src/main/java/co/yixiang/modules/coupons/web/param/YxCouponsQueryParam.java
@@ -1,11 +1,10 @@
 package co.yixiang.modules.coupons.web.param;
 
+import co.yixiang.common.web.param.QueryParam;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import co.yixiang.common.web.param.QueryParam;
 
 /**
  * <p>
@@ -17,9 +16,11 @@ import co.yixiang.common.web.param.QueryParam;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ApiModel(value="YxCouponsQueryParam对象", description="本地生活, 卡券表查询参数")
+@ApiModel(value = "YxCouponsQueryParam对象", description = "本地生活, 卡券表查询参数")
 public class YxCouponsQueryParam extends QueryParam {
     private static final long serialVersionUID = 1L;
     @ApiModelProperty(value = "店铺id")
     private Integer storeId;
+    @ApiModelProperty(value = "卡券分类id")
+    private Integer couponCategory;
 }
