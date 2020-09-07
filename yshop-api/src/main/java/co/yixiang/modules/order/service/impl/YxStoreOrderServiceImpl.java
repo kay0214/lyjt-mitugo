a16dd810da22c5d293612319b34ecf4d562279aa
nixiaoling <nixiaoling@hyjf.com>
2020-09-07 17:27:33 +0800

    添加购物车数量

--- yshop-api/src/main/java/co/yixiang/modules/order/service/impl/YxStoreOrderServiceImpl.java
+++ yshop-api/src/main/java/co/yixiang/modules/order/service/impl/YxStoreOrderServiceImpl.java
@@ -505,10 +505,12 @@ public class YxStoreOrderServiceImpl extends BaseServiceImpl<YxStoreOrderMapper,
      */
     @Override
     public void cancelOrderByTask(int orderId) {
+        log.info("---------- 超时取消订单 cancelOrderByTask ---------");
+
         YxStoreOrderQueryVo order = null;
         try {
             order = getYxStoreOrderById(orderId);
-            log.info("----------取消订单：{}"+order);
+            log.info("---------- 超时取消订单：{}"+order);
             if (ObjectUtil.isNull(order)) throw new ErrorRequestException("订单不存在");
 
             if (order.getIsDel() == OrderInfoEnum.CANCEL_STATUS_1.getValue()) throw new ErrorRequestException("订单已取消");
@@ -522,7 +524,7 @@ public class YxStoreOrderServiceImpl extends BaseServiceImpl<YxStoreOrderMapper,
             YxStoreOrder storeOrder = yxStoreOrderMapper.selectById(order.getId());
             storeOrder.setIsDel(OrderInfoEnum.CANCEL_STATUS_1.getValue());
 //            storeOrder.setId(order.getId());
-            log.info("----------保存取消结果：{}"+storeOrder);
+            log.info("---------- 保存取消结果：{}"+storeOrder);
             yxStoreOrderMapper.updateById(storeOrder);
         } catch (Exception e) {
             e.printStackTrace();
