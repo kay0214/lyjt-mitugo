d7947e3585ef9429068b7df7a0f7314d6726d9bd
cuijianan <cuijianan@hyjf.com>
2020-09-07 14:26:22 +0800

    添加分销配置页面

--- yshopmall_qd/src/views/shop/yxCommissionRate/index.vue
+++ yshopmall_qd/src/views/shop/yxCommissionRate/index.vue
@@ -2,139 +2,155 @@
   <div class="app-container">
     <!--工具栏-->
     <div class="head-container">
-      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission" />
       <!--表单组件-->
-      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
-        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-          <el-form-item label="主键">
-            <el-input v-model="form.id" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="平台抽成">
-            <el-input v-model="form.fundsRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="分享人">
-            <el-input v-model="form.shareRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="分享人上级">
-            <el-input v-model="form.shareParentRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="推荐人">
-            <el-input v-model="form.parentRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="商户">
-            <el-input v-model="form.merRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="合伙人">
-            <el-input v-model="form.partnerRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="拉新池">
-            <el-input v-model="form.referenceRate" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="是否删除（0：未删除，1：已删除）" prop="delFlag">
-            <el-input v-model="form.delFlag" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="创建人">
-            <el-input v-model="form.createUserId" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="修改人">
-            <el-input v-model="form.updateUserId" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="创建时间" prop="createTime">
-            <el-input v-model="form.createTime" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="更新时间" prop="updateTime">
-            <el-input v-model="form.updateTime" style="width: 370px;" />
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button type="text" @click="crud.cancelCU">取消</el-button>
-          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
-        </div>
-      </el-dialog>
-      <!--表格渲染-->
-      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
-        <el-table-column type="selection" width="55" />
-        <el-table-column v-if="columns.visible('id')" prop="id" label="主键" />
-        <el-table-column v-if="columns.visible('fundsRate')" prop="fundsRate" label="平台抽成" />
-        <el-table-column v-if="columns.visible('shareRate')" prop="shareRate" label="分享人" />
-        <el-table-column v-if="columns.visible('shareParentRate')" prop="shareParentRate" label="分享人上级" />
-        <el-table-column v-if="columns.visible('parentRate')" prop="parentRate" label="推荐人" />
-        <el-table-column v-if="columns.visible('merRate')" prop="merRate" label="商户" />
-        <el-table-column v-if="columns.visible('partnerRate')" prop="partnerRate" label="合伙人" />
-        <el-table-column v-if="columns.visible('referenceRate')" prop="referenceRate" label="拉新池" />
-        <el-table-column v-if="columns.visible('delFlag')" prop="delFlag" label="是否删除（0：未删除，1：已删除）" />
-        <el-table-column v-if="columns.visible('createUserId')" prop="createUserId" label="创建人" />
-        <el-table-column v-if="columns.visible('updateUserId')" prop="updateUserId" label="修改人" />
-        <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="创建时间">
-          <template slot-scope="scope">
-            <span>{{ parseTime(scope.row.createTime) }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column v-if="columns.visible('updateTime')" prop="updateTime" label="更新时间">
-          <template slot-scope="scope">
-            <span>{{ parseTime(scope.row.updateTime) }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column v-permission="['admin','yxCommissionRate:edit','yxCommissionRate:del']" label="操作" width="150px" align="center">
-          <template slot-scope="scope">
-            <udOperation
-              :data="scope.row"
-              :permission="permission"
-            />
-          </template>
-        </el-table-column>
-      </el-table>
-      <!--分页组件-->
-      <pagination />
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="150px"> 
+          <el-row :gutter='20'>
+            <el-col :span='6'>
+              <el-form-item label="平台抽成" prop='fundsRate'>
+                <el-input class='percent' v-model="form.fundsRate" />
+              </el-form-item>
+              <el-form-item label="分享人" prop='shareRate'>
+                <el-input class='percent' v-model="form.shareRate" />
+              </el-form-item>
+              <el-form-item label="商户" prop='merRate'>
+                <el-input class='percent score' v-model="form.merRate"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span='6'>
+              <el-form-item label="拉新池" prop='referenceRate'>
+                <el-input class='percent' v-model="form.referenceRate" />
+              </el-form-item>
+              <el-form-item label="分享人上级抽成" prop='shareParentRate'>
+                <el-input class='percent' v-model="form.shareParentRate" />
+              </el-form-item>
+              <el-form-item label="合伙人" prop='partnerRate'>
+                <el-input class='percent score' v-model="form.partnerRate" />
+              </el-form-item>
+            </el-col>
+            <el-col :span='6'>
+              <el-form-item label="购买人上级抽成" prop='parentRate'>
+                <el-input class='percent' v-model="form.parentRate" />
+              </el-form-item>  
+            </el-col>
+          </el-row>  
+      </el-form>   
+        <el-col slot="footer" :offset='10' class="dialog-footer" style='marginTop:30px'>
+          <el-button size='medium' :loading="sublLoading" type="primary" @click="submit">保存</el-button>
+        </el-col>   
     </div>
   </div>
 </template>
 
 <script>
-import crudYxCommissionRate from '@/api/yxCommissionRate'
-import CRUD, { presenter, header, form, crud } from '@crud/crud'
-import rrOperation from '@crud/RR.operation'
-import crudOperation from '@crud/CRUD.operation'
-import udOperation from '@crud/UD.operation'
-import pagination from '@crud/Pagination'
-import MaterialList from "@/components/material/index";
-
-// crud交由presenter持有
-const defaultCrud = CRUD({ title: '分佣配置', url: 'api/yxCommissionRate', sort: 'id,desc', crudMethod: { ...crudYxCommissionRate }})
-const defaultForm = { id: null, fundsRate: null, shareRate: null, shareParentRate: null, parentRate: null, merRate: null, partnerRate: null, referenceRate: null, delFlag: null, createUserId: null, updateUserId: null, createTime: null, updateTime: null }
+import { edit } from '@/api/yxCommissionRate'
 export default {
   name: 'YxCommissionRate',
-  components: { pagination, crudOperation, rrOperation, udOperation ,MaterialList},
-  mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
   data() {
     return {
-
+      sublLoading:false,
       permission: {
-        add: ['admin', 'yxCommissionRate:add'],
-        edit: ['admin', 'yxCommissionRate:edit'],
-        del: ['admin', 'yxCommissionRate:del']
+        add: ['admin', 'yxCommissionRate:edit'],
+        edit: ['admin', 'yxCommissionRate:edit']
       },
+      form: {},
       rules: {
-        delFlag: [
-          { required: true, message: '是否删除（0：未删除，1：已删除）不能为空', trigger: 'blur' }
+        fundsRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
         ],
-        createTime: [
-          { required: true, message: '创建时间不能为空', trigger: 'blur' }
+        shareRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
         ],
-        updateTime: [
-          { required: true, message: '更新时间不能为空', trigger: 'blur' }
-        ]
-      }    }
+        shareParentRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
+        ],
+        parentRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
+        ],
+        merRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
+        ],
+        partnerRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
+        ],
+        referenceRate: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          {
+            pattern: /^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/,  //正则
+            message: '请输入0~100的数字，最多两位小数',
+            trigger: 'blur'
+          },
+        ],
+      }    
+    }
   },
   watch: {
   },
   methods: {
-    // 获取数据前设置好接口地址
-    [CRUD.HOOK.beforeRefresh]() {
-      return true
-    }, // 新增与编辑前做的操作
-    [CRUD.HOOK.afterToCU](crud, form) {
+    async submit() {
+       this.$refs.form.validate(ret=>{
+        if(!ret){
+          return 
+        }else{
+          if(this.form.fundsRate*1+this.form.shareRate*1+this.form.
+  shareParentRate*1+this.form.parentRate*1+this.form.merRate*1+this.form.
+  partnerRate*1+this.form.referenceRate*1===100){            
+            this.sublLoading = true
+            edit(this.form.referenceRate).then(res => {
+              this.sublLoading = false
+              this.$notify({
+                title: '设置成功',
+                type: 'success',
+                duration: 2500
+              })
+            }).catch(err => {
+              console.log(err)
+              return
+              this.sublLoading = false
+              this.$notify({
+                title: '设置失败：'+err.response.data.message,
+                type: 'success',
+                duration: 2500
+              })
+            })
+          }else{
+            this.$notify({
+              title: '添加失败：平台抽成+拉新池+分享人+分享人上级抽成+购买人上级抽成+商户+合伙人=100%',
+              type: 'error',
+              duration: 2500
+            })
+          }
+        }
+      })      
     },
   }
 }
@@ -144,17 +160,17 @@ export default {
 </script>
 
 <style scoped>
-  .table-img {
-    display: inline-block;
-    text-align: center;
-    background: #ccc;
-    color: #fff;
-    white-space: nowrap;
-    position: relative;
-    overflow: hidden;
-    vertical-align: middle;
-    width: 32px;
-    height: 32px;
-    line-height: 32px;
+  .percent{
+    width:60%;
+  }
+  .percent::after {
+    content:"%";
+    position: absolute;
+    right:-20px;
+  }
+  .score::after{
+    content:'% 积分';
+    position: absolute;
+    right:-50px;
   }
 </style>
