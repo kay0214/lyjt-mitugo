caca222995a943d47b0b9f13cbd30fdd37208634
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-09-08 18:44:09 +0800

    【蜜兔】 商品购买增加资金明细

--- yshop-api/src/main/java/co/yixiang/modules/coupons/service/impl/YxCouponOrderServiceImpl.java
+++ yshop-api/src/main/java/co/yixiang/modules/coupons/service/impl/YxCouponOrderServiceImpl.java
@@ -778,7 +778,7 @@ public class YxCouponOrderServiceImpl extends BaseServiceImpl<YxCouponOrderMappe
         merBill.setType(BillDetailEnum.TYPE_8.getValue());
         merBill.setNumber(truePrice);
         // 目前只支持微信付款、没有余额
-        merBill.setBalance(updateSystemUser.getWithdrawalAmount());
+        merBill.setBalance(systemUser.getWithdrawalAmount().add(truePrice));
         merBill.setAddTime(DateUtils.getNowTime());
         merBill.setStatus(1);
         merBill.setMerId(yxCouponOrder.getMerId());
