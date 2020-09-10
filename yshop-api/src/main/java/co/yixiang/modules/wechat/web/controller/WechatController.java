33d6af37010230b6c7e0717d1fa30332c29d342c
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-09-10 15:18:39 +0800

    【蜜兔】 卡券退款逻辑修改

--- yshop-api/src/main/java/co/yixiang/modules/wechat/web/controller/WechatController.java
+++ yshop-api/src/main/java/co/yixiang/modules/wechat/web/controller/WechatController.java
@@ -253,10 +253,15 @@ public class WechatController extends BaseController {
             // 该笔资金实际到账
             SystemUser updateSystemUser = new SystemUser();
             BigDecimal truePrice = yxCouponOrder.getTotalPrice().subtract(yxCouponOrder.getCommission());
+            if(refundFee.compareTo(truePrice) > 0) {
+                updateSystemUser.setTotalAmount(truePrice.multiply(new BigDecimal(-1)));
+                updateSystemUser.setWithdrawalAmount(truePrice.multiply(new BigDecimal(-1)));
+            } else {
+                updateSystemUser.setTotalAmount(refundFee.multiply(new BigDecimal(-1)));
+                updateSystemUser.setWithdrawalAmount(refundFee.multiply(new BigDecimal(-1)));
+            }
             updateSystemUser.setId(systemUser.getId());
-            updateSystemUser.setTotalAmount(systemUser.getTotalAmount().subtract(truePrice));
-            updateSystemUser.setWithdrawalAmount(systemUser.getWithdrawalAmount().subtract(truePrice));
-            this.systemUserService.updateById(updateSystemUser);
+            this.systemUserService.updateUserTotal(updateSystemUser);
 
             // 插入商户资金明细
             YxUserBill merBill = new YxUserBill();
