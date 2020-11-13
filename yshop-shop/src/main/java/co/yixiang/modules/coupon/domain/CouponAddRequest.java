0a936a3204ab605dc1298c40445af9cd784d7afe
nixiaoling <nixiaoling@hyjf.com>
2020-11-13 15:27:53 +0800

    admin卡券修改

--- yshop-shop/src/main/java/co/yixiang/modules/coupon/domain/CouponAddRequest.java
+++ yshop-shop/src/main/java/co/yixiang/modules/coupon/domain/CouponAddRequest.java
@@ -157,7 +157,7 @@ public class CouponAddRequest implements Serializable {
     private Integer sort;
 
     /** 船只系列id */
-    @NotNull(message = "请选择船只系列")
+//    @NotNull(message = "请选择船只系列")
     private Integer seriesId;
 
 
@@ -166,12 +166,12 @@ public class CouponAddRequest implements Serializable {
 
 
     /** 合同模板id */
-    @NotNull(message = "合同模板")
+//    @NotNull(message = "合同模板")
     private Integer tempId;
 
 
     /** 乘客人数 */
-    @NotNull(message = "乘客人数")
+//    @NotNull(message = "乘客人数")
     private Integer passengersNum;
 
 
