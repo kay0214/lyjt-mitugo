68b9428465df9e6432b0dfa2dc3660ec91ad07e1
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-11-13 16:27:31 +0800

    【蜜兔】 查询条件优化

--- yshop-api/src/main/java/co/yixiang/modules/system/web/param/YxHotConfigQueryParam.java
+++ yshop-api/src/main/java/co/yixiang/modules/system/web/param/YxHotConfigQueryParam.java
@@ -1,10 +1,10 @@
 package co.yixiang.modules.system.web.param;
 
+import co.yixiang.common.web.param.QueryParam;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import co.yixiang.common.web.param.QueryParam;
 
 /**
  * <p>
@@ -16,7 +16,10 @@ import co.yixiang.common.web.param.QueryParam;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ApiModel(value="YxHotConfigQueryParam对象", description="HOT配置表查询参数")
+@ApiModel(value = "YxHotConfigQueryParam对象", description = "HOT配置表查询参数")
 public class YxHotConfigQueryParam extends QueryParam {
     private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "是否删除（0：未删除，1：已删除）")
+    private Integer delFlag = 0;
 }
