4dc88f174e2f62932b29df2f933402890c4e3821
cuijianan <cuijianan@hyjf.com>
2020-11-16 16:20:41 +0800

    add yxmerchantsSettlement page

--- yshopmall_qd/src/views/shopConfig/yxMerchantsSettlement/index.vue
+++ yshopmall_qd/src/views/shopConfig/yxMerchantsSettlement/index.vue
@@ -3,84 +3,74 @@
     <!--工具栏-->
     <div class="head-container">
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <el-input v-model="query.userName" clearable size="small" placeholder="请输入公司名"
+                style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+      <el-input v-model="query.contactsName" clearable size="small" placeholder="请输入联系人"
+                style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+      <el-input v-model="query.phone" clearable size="small" placeholder="请输入联系电话"
+                style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+      <el-select v-model="query.status" clearable placeholder="请选择状态"
+                 style="width: 200px;" class="filter-item">
+        <el-option
+          v-for="item in statusList"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="crud.toQuery">搜索</el-button>
       <crudOperation :permission="permission" />
       <!--表单组件-->
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
         <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-          <el-form-item label="id">
-            <el-input v-model="form.id" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="公司名" prop="companyName">
+          <el-form-item label="公司名">
             <el-input v-model="form.companyName" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="联系人" prop="contactsName">
+          <el-form-item label="联系人">
             <el-input v-model="form.contactsName" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="联系电话" prop="phone">
+          <el-form-item label="联系电话">
             <el-input v-model="form.phone" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="联系地址" prop="address">
+          <el-form-item label="联系地址">
             <el-input v-model="form.address" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="说明" prop="explain">
+          <el-form-item label="说明">
             <el-input v-model="form.explain" style="width: 370px;" />
           </el-form-item>
           <el-form-item label="备注" prop="remark">
-            <el-input v-model="form.remark" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="状态：0：待联系，1：有意向，2：已拒绝" prop="status">
-            <el-input v-model="form.status" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="是否删除（0：未删除，1：已删除）" prop="delFlag">
-            <el-input v-model="form.delFlag" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="创建人">
-            <el-input v-model="form.createUserId" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="修改人">
-            <el-input v-model="form.updateUserId" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="创建时间" prop="createTime">
-            <el-input v-model="form.createTime" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="更新时间" prop="updateTime">
-            <el-input v-model="form.updateTime" style="width: 370px;" />
+            <el-input v-model="form.remark" type="textarea"
+                      :row='10' style="width: 370px;"
+                      show-word-limit  maxlength="200"/>
           </el-form-item>
         </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button type="text" @click="crud.cancelCU">取消</el-button>
-          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        <div slot="footer" class="dialog-footer"  v-if="!scope.row.status">
+          <el-button v-if="scope.row.status" type="text" @click="crud.cancelCU">关闭</el-button>
+          <el-button v-if="!scope.row.status" :loading="crud.cu === 2" type="primary" @click="()=>{form.status=1;crud.submitCU()}">有意向</el-button>
+          <el-button v-if="!scope.row.status" :loading="crud.cu === 2" type="primary" @click="()=>{form.status=2;crud.submitCU()}">已拒绝</el-button>
         </div>
       </el-dialog>
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="selection" width="55" />
-        <el-table-column v-if="columns.visible('id')" prop="id" label="id" />
         <el-table-column v-if="columns.visible('companyName')" prop="companyName" label="公司名" />
         <el-table-column v-if="columns.visible('contactsName')" prop="contactsName" label="联系人" />
         <el-table-column v-if="columns.visible('phone')" prop="phone" label="联系电话" />
-        <el-table-column v-if="columns.visible('address')" prop="address" label="联系地址" />
-        <el-table-column v-if="columns.visible('explain')" prop="explain" label="说明" />
-        <el-table-column v-if="columns.visible('remark')" prop="remark" label="备注" />
-        <el-table-column v-if="columns.visible('status')" prop="status" label="状态：0：待联系，1：有意向，2：已拒绝" />
-        <el-table-column v-if="columns.visible('delFlag')" prop="delFlag" label="是否删除（0：未删除，1：已删除）" />
-        <el-table-column v-if="columns.visible('createUserId')" prop="createUserId" label="创建人" />
-        <el-table-column v-if="columns.visible('updateUserId')" prop="updateUserId" label="修改人" />
-        <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="创建时间">
+        <el-table-column v-if="columns.visible('status')" prop="status" label="状态" >
           <template slot-scope="scope">
-            <span>{{ parseTime(scope.row.createTime) }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column v-if="columns.visible('updateTime')" prop="updateTime" label="更新时间">
-          <template slot-scope="scope">
-            <span>{{ parseTime(scope.row.updateTime) }}</span>
+            <span @click="update(scope.row)">
+              {{ transferLabel(scope.row.status,statusList) }}</span>
           </template>
         </el-table-column>
         <el-table-column v-permission="['admin','yxMerchantsSettlement:edit','yxMerchantsSettlement:del']" label="操作" width="150px" align="center">
           <template slot-scope="scope">
+            <el-button v-if="!scope.row.status" v-permission="permission.edit" size="mini" type="primary"
+                       icon="el-icon-edit" @click="crud.toEdit(scope.row)" />
+            <el-button v-else v-permission="permission.edit" size="mini" type="primary"
+                       icon="el-icon-view" @click="crud.toEdit(scope.row)" plain/>
             <udOperation
               :data="scope.row"
-              :permission="permission"
+              :permission="permissionDel"
             />
           </template>
         </el-table-column>
@@ -101,7 +91,13 @@ import pagination from '@crud/Pagination'
 import MaterialList from "@/components/material";
 
 // crud交由presenter持有
-const defaultCrud = CRUD({ title: '商家入驻', url: 'api/yxMerchantsSettlement', sort: 'id,desc', crudMethod: { ...crudYxMerchantsSettlement }})
+const defaultCrud = CRUD({ title: '商家入驻', url: 'api/yxMerchantsSettlement', sort: 'id,desc',
+  optShow: {
+    add: false,
+    edit: false,
+    del: false,
+    download: true
+  },crudMethod: { ...crudYxMerchantsSettlement }})
 const defaultForm = { id: null, companyName: null, contactsName: null, phone: null, address: null, explain: null, remark: null, status: null, delFlag: null, createUserId: null, updateUserId: null, createTime: null, updateTime: null }
 export default {
   name: 'YxMerchantsSettlement',
@@ -109,10 +105,18 @@ export default {
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
   data() {
     return {
-      
+      statusList:[
+        {value:0,label:'待联系'},
+        {value:1,label:'有意向'},
+        {value:2,label:'已拒绝'}
+      ],
       permission: {
         add: ['admin', 'yxMerchantsSettlement:add'],
         edit: ['admin', 'yxMerchantsSettlement:edit'],
+        del: ['admin', 'yxMerchantsSettlement:del']
+      },
+      permissionDel:{
+        edit: [''],
         del: ['admin', 'yxMerchantsSettlement:del']
       },
       rules: {
@@ -153,6 +157,10 @@ export default {
   methods: {
     // 获取数据前设置好接口地址
     [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }, // "新建/编辑" 验证 - 之前
+    [CRUD.HOOK.beforeValidateCU](crud) {
+      console.log(crud.form);
       return true
     }, // 新增与编辑前做的操作
     [CRUD.HOOK.afterToCU](crud, form) {
