9f4c04fd9ecef083f69893a6850e447236c2d8b3
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-11-17 10:19:13 +0800

    【蜜兔】 商家入驻优化1

--- yshop-shop/src/main/java/co/yixiang/modules/shopConfig/domain/YxMerchantsSettlement.java
+++ yshop-shop/src/main/java/co/yixiang/modules/shopConfig/domain/YxMerchantsSettlement.java
@@ -59,8 +59,6 @@ public class YxMerchantsSettlement implements Serializable {
 
 
     /** 是否删除（0：未删除，1：已删除） */
-    @TableLogic
-    @TableField(fill=FieldFill.INSERT_UPDATE)
     private Integer delFlag;
 
 
@@ -73,12 +71,10 @@ public class YxMerchantsSettlement implements Serializable {
 
 
     /** 创建时间 */
-    @TableField(fill= FieldFill.INSERT)
     private Timestamp createTime;
 
 
     /** 更新时间 */
-    @TableField(fill= FieldFill.INSERT_UPDATE)
     private Timestamp updateTime;
 
 
