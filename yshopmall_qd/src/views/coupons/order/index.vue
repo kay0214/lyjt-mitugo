5e79f41b99e3b09aa104f5ae77dd761469a1f23b
cuijianan <cuijianan@hyjf.com>
2020-11-18 15:12:09 +0800

    add search btn

--- yshopmall_qd/src/views/coupons/order/index.vue
+++ yshopmall_qd/src/views/coupons/order/index.vue
@@ -18,21 +18,22 @@
       <div class="head-container">
         <div>
           <!-- 搜索 -->
+          <el-input v-model="query.storeName" clearable placeholder="输入商品名称" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
           <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
           <el-select v-model="query.orderType" clearable placeholder="类型" class="filter-item" style="width: 130px">
             <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
-          <!-- <el-date-picker
-            v-model="createTime"
+           <el-date-picker
+            v-model="query.createTime"
             :default-time="['00:00:00','23:59:59']"
             type="daterange"
             range-separator=":"
             size="small"
             class="date-item"
-            value-format="yyyy-MM-dd HH:mm:ss"
+            value-format="yyyy-MM-dd"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
-          /> -->
+          />
           <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="crud.toQuery">搜索</el-button>
           <!-- 新增 -->
           <el-button
--- yshopmall_qd/src/views/shop/order/index.vue
+++ yshopmall_qd/src/views/shop/order/index.vue
@@ -35,6 +35,7 @@
       <div class="head-container">
 
         <!-- 搜索 -->
+        <el-input v-model="query.storeName" clearable placeholder="输入商品名称" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
         <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
         <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
           <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
@@ -48,13 +49,13 @@
           />
         </el-select>-->
         <el-date-picker
-          v-model="createTime"
+          v-model="query.createTime"
           :default-time="['00:00:00','23:59:59']"
           type="daterange"
           range-separator=":"
           size="small"
           class="date-item"
-          value-format="yyyy-MM-dd HH:mm:ss"
+          value-format="yyyy-MM-dd"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
         />
