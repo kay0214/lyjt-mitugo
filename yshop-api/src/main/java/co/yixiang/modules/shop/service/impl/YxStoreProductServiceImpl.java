8989c9db7e2ca640017aaf710f5f39d4b5527b44
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-11-19 14:54:15 +0800

    【蜜兔】  本地生活增加查询详情接口

--- yshop-api/src/main/java/co/yixiang/modules/shop/service/impl/YxStoreProductServiceImpl.java
+++ yshop-api/src/main/java/co/yixiang/modules/shop/service/impl/YxStoreProductServiceImpl.java
@@ -255,7 +255,7 @@ public class YxStoreProductServiceImpl extends BaseServiceImpl<YxStoreProductMap
     @Override
     public ProductDTO goodsDetail(int id, int type, int uid, String latitude, String longitude) {
         QueryWrapper<YxStoreProduct> wrapper = new QueryWrapper<>();
-        wrapper.eq("is_del", 0).eq("is_show", 1).eq("id", id);
+        wrapper.eq("is_del", 0).eq("id", id);
         YxStoreProduct storeProduct = yxStoreProductMapper.selectOne(wrapper);
         if (ObjectUtil.isNull(storeProduct)) {
             throw new ErrorRequestException("商品不存在或已下架");
