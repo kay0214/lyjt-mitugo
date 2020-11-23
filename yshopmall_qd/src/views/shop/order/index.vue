81f4111a2b02f7ad42d3ba9e48ad0d664e1ca1e0
cuijianan <cuijianan@hyjf.com>
2020-11-23 10:27:27 +0800

    del unused search btn

--- yshopmall_qd/src/views/shop/order/index.vue
+++ yshopmall_qd/src/views/shop/order/index.vue
@@ -35,7 +35,7 @@
       <div class="head-container">
 
         <!-- 搜索 -->
-        <el-input v-model="query.storeName" clearable placeholder="输入商品名称" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
+<!--        <el-input v-model="query.storeName" clearable placeholder="输入商品名称" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />-->
         <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
         <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
           <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
