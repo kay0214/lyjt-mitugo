1fe06f650b2c611e55f26d4a4427ae8495110ab1
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-11-23 18:23:16 +0800

    【蜜兔】 查询条件优化2

--- yshop-shop/src/main/java/co/yixiang/modules/shop/service/dto/YxStoreProductReplyQueryCriteria.java
+++ yshop-shop/src/main/java/co/yixiang/modules/shop/service/dto/YxStoreProductReplyQueryCriteria.java
@@ -1,7 +1,6 @@
 /**
  * Copyright (C) 2018-2020
  * All rights reserved, Designed By www.yixiang.co
-
  */
 package co.yixiang.modules.shop.service.dto;
 
@@ -9,11 +8,14 @@ import co.yixiang.annotation.Query;
 import lombok.Data;
 
 /**
-* @author hupeng
-* @date 2020-05-12
-*/
+ * @author hupeng
+ * @date 2020-05-12
+ */
 @Data
-public class YxStoreProductReplyQueryCriteria extends BaseCriteria{
+public class YxStoreProductReplyQueryCriteria extends BaseCriteria {
     @Query
     private Integer isDel;
+    private Integer isReply;
+    private String storeName;
+    private String username;
 }
