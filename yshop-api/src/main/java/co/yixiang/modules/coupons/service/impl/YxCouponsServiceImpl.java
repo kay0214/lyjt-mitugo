5ed15e7f812c5c9afc8d5b3cd2f72869c31b90ad
nixiaoling <nixiaoling@hyjf.com>
2020-11-24 17:38:27 +0800

    价格配置，bug修复

--- yshop-api/src/main/java/co/yixiang/modules/coupons/service/impl/YxCouponsServiceImpl.java
+++ yshop-api/src/main/java/co/yixiang/modules/coupons/service/impl/YxCouponsServiceImpl.java
@@ -544,7 +544,7 @@ public class YxCouponsServiceImpl extends BaseServiceImpl<YxCouponsMapper, YxCou
                 .eq(YxCouponsPriceConfig::getCouponId, couponId)
                 .eq(YxCouponsPriceConfig::getDelFlag, 0)
                 .le(YxCouponsPriceConfig::getStartDate, nowDate)
-                .ge(YxCouponsPriceConfig::getEndDate, nowDate).orderByDesc(YxCouponsPriceConfig::getCreateTime);
+                .ge(YxCouponsPriceConfig::getEndDate, nowDate).orderByDesc(YxCouponsPriceConfig::getId);
         List<YxCouponsPriceConfig> couponsPriceConfigList = yxCouponsPriceConfigService.list(queryWrapper);
         if (!CollectionUtils.isEmpty(couponsPriceConfigList)) {
             return couponsPriceConfigList.get(0);
