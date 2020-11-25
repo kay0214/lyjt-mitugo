06eaf7253477b203d5b2a8e4f1c13a3a401fed60
PC-LIUSHOUYI <liushouyi@hyjf.com>
2020-11-25 15:23:18 +0800

    【蜜兔】  修改船员签到表

--- yshop-shop/src/main/java/co/yixiang/modules/shipManage/service/dto/YxCrewSignDto.java
+++ yshop-shop/src/main/java/co/yixiang/modules/shipManage/service/dto/YxCrewSignDto.java
@@ -1,22 +1,23 @@
 /**
-* Copyright (C) 2018-2020
-* All rights reserved, Designed By www.yixiang.co
-* 注意：
-* 本软件为www.yixiang.co开发研制，未经购买不得使用
-* 购买后可获得全部源代码（禁止转卖、分享、上传到码云、github等开源平台）
-* 一经发现盗用、分享等行为，将追究法律责任，后果自负
-*/
+ * Copyright (C) 2018-2020
+ * All rights reserved, Designed By www.yixiang.co
+ * 注意：
+ * 本软件为www.yixiang.co开发研制，未经购买不得使用
+ * 购买后可获得全部源代码（禁止转卖、分享、上传到码云、github等开源平台）
+ * 一经发现盗用、分享等行为，将追究法律责任，后果自负
+ */
 package co.yixiang.modules.shipManage.service.dto;
 
 import lombok.Data;
-import java.sql.Timestamp;
-import java.math.BigDecimal;
+
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 /**
-* @author nxl
-* @date 2020-11-04
-*/
+ * @author nxl
+ * @date 2020-11-04
+ */
 @Data
 public class YxCrewSignDto implements Serializable {
 
@@ -69,4 +70,6 @@ public class YxCrewSignDto implements Serializable {
 
     private Timestamp updateTime;
 
+    /** 用户名*/
+    private String nickName;
 }
