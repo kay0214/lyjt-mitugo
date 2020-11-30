86e043bb13687db07ee22a74e7b464a5b82edaae
cuijianan <cuijianan@hyjf.com>
2020-11-30 15:20:38 +0800

    label width add

--- yshopmall_qd/src/views/ship/shipSeries/index.vue
+++ yshopmall_qd/src/views/ship/shipSeries/index.vue
@@ -7,8 +7,8 @@
       <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="crud.toQuery">搜索</el-button>
       <crudOperation :permission="permission" />
       <!--表单组件-->
-      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
-        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="540px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="110px">
           <el-form-item label="系列名称" prop="seriesName">
             <el-input v-model="form.seriesName" style="width: 370px;" maxlength="15"/>
           </el-form-item>
