74d8a012ee25c452be259dbe64bbad3bec34e330
nixiaoling <nixiaoling@hyjf.com>
2020-12-02 16:51:39 +0800

    bug修复

--- yshop-api/src/main/java/co/yixiang/modules/ship/service/impl/YxShipInfoServiceImpl.java
+++ yshop-api/src/main/java/co/yixiang/modules/ship/service/impl/YxShipInfoServiceImpl.java
@@ -563,9 +563,9 @@ public class YxShipInfoServiceImpl extends BaseServiceImpl<YxShipInfoMapper, YxS
         // 以下为测试
         MailAccount account = new MailAccount();
         // 企业
-//        account.setHost("smtp.qiye.163.com");
+        account.setHost("smtp.qiye.163.com");
         //个人
-        account.setHost("smtp.163.com");
+//        account.setHost("smtp.163.com");
         account.setPort(25);
         account.setAuth(true);
         //以下需配置
